68ad6c28180dc7d9710eb58803b8d0d181d6e69e
Farmankhan733 <107101437+Farmankhan733@users.noreply.github.com>
2024-08-13 14:21:15 +0530

    upload chapter ps 1 in git

---
+++ Chapter 2/02_datatype.py
@@ -0,0 +1,9 @@
+a = 1 # a is an integer 
+
+b = 5.22 # b is an floating point number
+
+c = "farman" # c is an string 
+
+d = False # d is an boolean variable 
+
+e = None # e is an none type variable
---
+++ Chapter 2/03_operators.py
@@ -0,0 +1,36 @@
+# Arithmetic Operators
+
+a = 7
+b = 4 
+c = a + b 
+print(c)
+
+# Assignemt Operators 
+a = 4-2 # Assign 4-3 in a 
+print(a)
+b = 6
+b += 3 # Increment the value of b by 3 and then assign it to b 
+print(b)
+
+# Comparison Operators 
+
+d = 5<=5 
+print(d)
+
+# Logical Operators
+
+e = True or False
+
+# Truth table of 'or' 
+print("True or False is", True or False)
+print("False or True is", False or True)
+print("True or True is", True or True)
+print("False or False is", False or False)
+
+# Truth table of 'and'
+print("True and False is", True and False)
+print("False and True is", False and True)
+print("True and True is", True and True)
+print("False and False is", False and False)
+
+print(not(True))
